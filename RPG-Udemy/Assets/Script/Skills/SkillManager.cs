6925c9b452c1de4958c82da6044a5faebad53870
VinhTienDo <ledoanvinh96@gmail.com>
2024-05-22 22:44:14 +0700

    Crystal spawn, teleport and explode

--- RPG-Udemy/Assets/Script/Skills/SkillManager.cs
+++ RPG-Udemy/Assets/Script/Skills/SkillManager.cs
@@ -10,6 +10,7 @@ public class SkillManager : MonoBehaviour
     public Clone_Skill clone { get; private set; }
     public Sword_Skill sword { get; private set; }
     public BlackHole_Skill blackHole { get; private set; }
+    public Crystal_Skill crystal { get; private set; }
 
 
     private void Awake()
@@ -26,5 +27,6 @@ public class SkillManager : MonoBehaviour
         clone = GetComponent<Clone_Skill>();
         sword = GetComponent<Sword_Skill>();
         blackHole = GetComponent<BlackHole_Skill>();
+        crystal = GetComponent<Crystal_Skill>();
     }
 }
